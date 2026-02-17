6a148ef90713817ad456d7f55e7f3a76e41e51cf
Juliana Valbuena <julianaaavz@gmail.com>
2026-02-17 13:27:56 -0500

    Update data.py

--- data.py
+++ data.py
@@ -1,1 +1,15 @@
+# Products
+products = ["A", "B"]
+
+# Profit per unit
+profit = {"A": 40, "B": 30}
+
+# Resource availability
+labor_available = 100
+material_available = 90
+
+# Resource consumption per unit
+labor_required = {"A": 2, "B": 1}
+
+material_required = {"A": 1, "B": 3}
 
